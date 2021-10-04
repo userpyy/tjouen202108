151fec960978ed2d00e390a4af3d08d954f33077
User <User@DESKTOP-1UVCHFB>
2021-10-04 20:08:36 +0900

    test

---
+++ test02/src/test02/memberTest.java
@@ -0,0 +1,18 @@
+package test02;
+
+public class memberTest {
+
+	public static void main(String[] args) {
+
+		Member m = new Member("전무", 28);
+		System.out.println(m);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
